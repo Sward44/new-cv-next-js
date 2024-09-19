9a16213ff8e71f699a10d12e9814e72d40617b12
david <davidlaunay567@gmail.com>
2024-09-19 10:33:52 +0100

    Updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -10,5 +10,5 @@ export function middleware(request) {
 }
 export const config = {
   matcher:
-    "/((?!_app.js|_next|favicon.ico|android-chrome-icon-192x192.png|android-chrome-icon-512x512.png|apple-touch-icon-180x180.png|david-launay-icon.svg|david-launay-reseau-sociaux.webp|favicon-16x16.png|favicon-32x32.png|manifest.json|sitemap.xml|img|api).*)",
+    "/((?!_app.js|_next|favicon.ico|android-chrome-icon-192x192.png|android-chrome-icon-512x512.png|apple-touch-icon-180x180.png|david-launay-icon.svg|david-launay-reseau-sociaux.webp|favicon-16x16.png|favicon-32x32.png|manifest.json|sitemap.xml|img|api/**).*)",
 };
