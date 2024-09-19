22c05f0f2c7d234006fe9d91a23965a5ddd9e4d9
david <davidlaunay567@gmail.com>
2024-09-19 11:09:15 +0100

    Updated with configartion on Vercel

--- src/app/[locale]/api/user/route.js
+++ src/app/[locale]/api/user/route.js
@@ -3,20 +3,33 @@ import { connectMongoose } from "@/utils/Mongoose";
 import { UserModel } from "@/models/index";
 
 export const POST = async (req) => {
-  const body = await req.json();
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
+  }
+
   await connectMongoose();
   try {
+    const body = await req.json();
     const existingUser = await UserModel.findOne({ email: body.email }).exec();
     if (existingUser) {
       const userObject = existingUser.toObject();
       const { _id, createdAt, updatedAt, __v, ...existingUserWithout } =
         userObject;
-      return NextResponse.json(
-        { message: existingUserWithout },
+
+      return new NextResponse(
+        JSON.stringify({ message: existingUserWithout }),
         {
           status: 200,
           headers: {
             "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
           },
         }
       );
@@ -26,30 +39,37 @@ export const POST = async (req) => {
         done: body.done,
       });
       await newUser.save();
-
       const newUserObject = newUser.toObject();
       const { _id, createdAt, updatedAt, __v, ...newUserWithout } =
         newUserObject;
 
-      return NextResponse.json(
-        { message: newUserWithout },
-        {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Erreur de serveur" },
-      {
-        status: 500,
+      return new NextResponse(JSON.stringify({ message: newUserWithout }), {
+        status: 200,
         headers: {
           "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
         },
-      }
-    );
+      });
+    }
+  } catch (error) {
+    console.error("Error in user API route:", error);
+    return new NextResponse(JSON.stringify({ error: "Erreur de serveur" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
   }
+};
+
+export const OPTIONS = async () => {
+  return new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 };
