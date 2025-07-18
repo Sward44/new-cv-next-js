c2a1dd731ec944d688c2f2b56b4780a1885012ca
Sward44 <davidlaunay567@gmail.com>
2025-07-18 10:15:56 +0200

    Suppression de packages @vercel/analystics @vercel/speed-insights

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,5 +1,4 @@
 import "./globals.scss";
-import { Analytics } from "@vercel/analytics/react";
 import { mulish } from "@/fonts/fonts";
 import { availableLocales } from "@/utiltaire/i18n";
 import { getDictionary } from "./dictionaries";
@@ -103,10 +102,7 @@ export function generateStaticParams() {
 export default async function RootLayout({ children, params }) {
   return (
     <html lang={params.locale}>
-      <body className={mulish.variable}>
-        {children}
-        <Analytics />
-      </body>
+      <body className={mulish.variable}>{children}</body>
     </html>
   );
 }
